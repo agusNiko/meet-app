59e0be324d4235d0d4895336c2dce9e6375a692f
agusNiko <73887894+agusNiko@users.noreply.github.com>
2021-05-27 20:21:38 +0200

    Update handler.js

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -2,8 +2,8 @@ const { google } = require("googleapis");
 const OAuth2 = google.auth.OAuth2;
 const calendar = google.calendar("v3");
 
+// const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]; I didnt dind the scope ../auth/calendar.readonly thats why I add the ../auth/calendar.events.randonly
 const SCOPES = ["https://www.googleapis.com/auth/calendar.events.readonly"];
-// ../auth/calendar.readonly
 
 const credentials = {
   client_id: process.env.CLIENT_ID,
@@ -13,11 +13,8 @@ const credentials = {
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  redirect_uris: ["https://YOUR_GITHUB_USERNAME.github.io/meet/"],
-  javascript_origins: [
-    "https://YOUR_GITHUB_USERNAME.github.io",
-    "http://localhost:3000",
-  ],
+  redirect_uris: ["https://agusNiko.github.io/meet/"],
+  javascript_origins: ["https://agusNiko.github.io", "http://localhost:3000"],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
 const oAuth2Client = new google.auth.OAuth2(
@@ -26,21 +23,7 @@ const oAuth2Client = new google.auth.OAuth2(
   redirect_uris[0]
 );
 
-/**
- *
- * The first step in the OAuth process is to generate a URL so users can log in with
- * Google and be authorized to see your calendar. After logging in, they’ll receive a code
- * as a URL parameter.
- *
- */
 module.exports.getAuthURL = async () => {
-  /**
-   *
-   * Scopes array passed to the `scope` option. Any scopes passed must be enabled in the
-   * "OAuth consent screen" settings in your project on your Google Console. Also, any passed
-   *  scopes are the ones users will see when the consent screen is displayed to them.
-   *
-   */
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: "offline",
     scope: SCOPES,
