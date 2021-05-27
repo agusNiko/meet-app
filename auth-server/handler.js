d1a0867e8d7b12488f544fcc10ae7ee44a391e48
agusNiko <73887894+agusNiko@users.noreply.github.com>
2021-05-27 20:23:34 +0200

    auth server

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -13,7 +13,7 @@ const credentials = {
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  redirect_uris: ["https://agusNiko.github.io/meet/"],
+  redirect_uris: ["https://agusNiko.github.io/meet-app/"],
   javascript_origins: ["https://agusNiko.github.io", "http://localhost:3000"],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
