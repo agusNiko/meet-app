45462516c9fb52423308950adf8acb71069c5c82
agusNiko <73887894+agusNiko@users.noreply.github.com>
2021-06-10 18:07:50 +0200

    fixed locations bug

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -16,10 +16,8 @@ afterAll(() => {
 
 describe("show/hide an event details", () => {
   test("An event element is collapsed by default", async () => {
-    const eventDetails = await page.$(".Event .EventDetails");
-    const showLessButton = await page.$(".showLess");
-    expect(eventDetails).toBeDefined();
-    expect(showLessButton).toBeDefined();
+    await expect(page.$(".Event .EventDetails")).resolves.toBe(null);
+    await expect(page.$(".showLess")).resolves.toBe(null);
   });
   test("User can expand an event to see its details and the Show less Button", async () => {
     await page.click(".Event .showMore"); //Why do I need the ".Event"???
@@ -34,8 +32,10 @@ describe("show/hide an event details", () => {
 
   test("User can collapse an event to hide its details", async () => {
     await page.click(".Event .showLess");
-    const eventDetails = await page.$(".EventDetails");
-    console.log(eventDetails);
-    expect(eventDetails).toBeDefined(); //this is wrong It should fail with toBeDefined()
+    await expect(page.$(".EventDetails")).resolves.toBe(null);
   });
 });
+
+// describe("filter Events by city", () => {
+//   page.type("#mytextarea", "Hello");
+// });
