70e5a947454875993481e7eb91232485887d8119
agusNiko <73887894+agusNiko@users.noreply.github.com>
2021-06-25 06:10:23 +0200

    Update EventGenre.js

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -1,13 +1,12 @@
-import React, { useCallback, useState } from "react";
-import { PieChart, Pie, Cell } from "recharts";
+import React, { useEffect, useState } from "react";
+import { PieChart, Pie, Cell, ResponsiveContainer } from "recharts";
 
 export default function EventGenre({ events }) {
-  //  const data = [
-  //     { name: "Group A", value: 100 },
-  //     { name: "Group B", value: 300 },
-  //     { name: "Group C", value: 300 },
-  //     { name: "Group D", value: 200 },
-  //   ];
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setData(() => getData());
+  }, [events]);
 
   const genres = ["React", "JavaScript", "Node", "jQuery", "AngularJS"];
   const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
@@ -32,23 +31,53 @@ export default function EventGenre({ events }) {
     });
     return data;
   };
-  const data = getData();
-  return (
-    <PieChart width={400} height={400}>
-      <Pie
-        data={data}
-        cx={200}
-        cy={200}
-        labelLine={false}
-        // label={a}
-        outerRadius={80}
-        fill="#8884d8"
-        dataKey="value"
+
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+    index,
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+      <text
+        x={x}
+        y={y}
+        fill="white"
+        textAnchor={x > cx ? "start" : "end"}
+        dominantBaseline="central"
       >
-        {data.map((entry, index) => (
-          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-        ))}
-      </Pie>
-    </PieChart>
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
+
+  const pieChartData = getData();
+  return (
+    <ResponsiveContainer height={400}>
+      <PieChart width={400} height={400}>
+        <Pie
+          data={pieChartData}
+          cx={200}
+          cy={200}
+          labelLine={false}
+          label={renderCustomizedLabel}
+          outerRadius={80}
+          fill="#8884d8"
+          dataKey="value"
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+      </PieChart>
+    </ResponsiveContainer>
   );
 }
